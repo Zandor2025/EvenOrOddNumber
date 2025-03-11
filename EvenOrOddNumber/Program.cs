dacc2c304863cd09f89662622f6e339f84f80ca3
Zandor2025 <slawomir.sobkowiak@gmail.com>
2025-03-11 23:08:57 +0100

    Add project files.

---
+++ EvenOrOddNumber/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace EvenOrOddNumber
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                int number;
+                if (int.TryParse(input, out number))
+                {
+                    if (number % 2 == 0)
+                    {
+                        Console.WriteLine("To jest liczba przysta");
+                    }
+                    else
+                    {
+                        Console.WriteLine("To jest liczba nieparzysta");
+                    }
+                        Console.WriteLine("Czy chcesz sprawdzić kolejną liczbę? Wpisz 'tak' lub 'nie'");
+                    string answer = Console.ReadLine();
+                    if (answer == "tak")
+                    {
+                        Console.WriteLine("Podaj liczbę");
+                    }
+                    else if (answer == "nie")
+                    {
+                        Console.WriteLine("Dziękuję za skorzystanie z programu");
+                        break;
+                    }
+                    else if (answer != "nie")
+                        Console.WriteLine("Nie rozumiem decyzji, wprowadz jeszcze raz dycyzję");
+                     
+
+                    else if (answer != "tak")
+                        Console.WriteLine("Nie rozumiem decyzji 2, wprowadz jeszcze raz dycyzję");
+                  
+                                   }
+                    else
+                {
+                        Console.WriteLine("Wprowdz cyfrę");
+                }
+            }
+        }
+    }
+}
